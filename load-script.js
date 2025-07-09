e9a4e61a1b3459ba5d64872849d517a10d389ce6
ys-10k <vas11io@outlook.jp>
2025-07-09 22:02:26 +0900

    Add files via upload

--- load-script.js
+++ load-script.js
@@ -1,6 +1,19 @@
     // ページが読み込まれたとき（load）にスクロール位置をトップに戻す
+
     window.addEventListener('load', () => {
-      setTimeout(() => {
-        window.scrollTo(0, 0);
-      }, 50);
-    });
+    // スクロール位置を初期化
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 50);
+
+    // フェードアウト開始（3秒後）
+    setTimeout(() => {
+      const splash = document.getElementById('splash');
+      if (splash) {
+        splash.classList.add('fade-out');
+        setTimeout(() => {
+          splash.style.display = 'none';
+        }, 2000); // アニメーション時間と同じだけ待って非表示に
+      }
+    }, 3000); // 表示しておく時間
+  });
